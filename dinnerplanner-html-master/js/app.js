5a87ec556d6edad5f8a22d56cd6758f571123ccf
Tove Grimstad Bang <37261596+togrba@users.noreply.github.com>
2019-01-22 14:51:10 +0100

    Add files via upload
    app updated with instances

--- dinnerplanner-html-master/js/app.js
+++ dinnerplanner-html-master/js/app.js
@@ -1,14 +1,25 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
-	
-	// And create the instance of ExampleView
-	var exampleView = new ExampleView($("#exampleView"));
+
+	// And create the instance of ExampleView +++
+	// var exampleView = new ExampleView($("#exampleView"), model);			// NB jQuery
+
+	// OBS class names starts with capital letter, see js files under js -> view folder
+	// view files according to prototype provided
+	// var welcomeView = new WelcomeView($("#welcomeView"), model);
+	var sidebarView = new SidebarView($("#sidebarView"), model);
+	// var dishItemView = new DishItemView($("#dishItemView"), model);
+	var dishSearchView = new DishSearchView($("#dishSearchView"), model);
+	// var dishDetailsView = new DishDetailsView($("#dishDetailsView"), model);
+	// var dinnerStatusView = new DinnerStatusView($("#dinnerStatusView"), model);
+
+	//console.log(model);
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
 	 * use the $('someSelector') to search for elements in the whole HTML.
-	 * In other places you should limit the search only to the children 
+	 * In other places you should limit the search only to the children
 	 * of the specific view you're working with (see exampleView.js).
 	 */
 
