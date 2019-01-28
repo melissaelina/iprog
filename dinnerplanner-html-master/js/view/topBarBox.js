f06c810456b6c46def576d5e7cbbe72ba26e8977
togrba <togrba@gmail.com>
2019-01-28 17:10:02 +0100

    Updated template literals

--- dinnerplanner-html-master/js/view/topBarBox.js
+++ dinnerplanner-html-master/js/view/topBarBox.js
@@ -1,8 +1,9 @@
 var TopBarBox = function (container, model) {
-
-	container.append('<h2>My dinner: <span id="numberOfGuests"></span> people</h2>'+
-  '<div id="buttonplacement">'+
-  '<button class="button"><b>Go back and edit dinner</b></button>'+
-  '</div>');
-
+	container.append(`
+		<h2>My dinner: <span id="numberOfGuests"></span> people
+		</h2>
+		<div id="buttonplacement">
+			<button class="button"><b>Go back and edit dinner</b></button>
+		</div>
+		`);
 }
