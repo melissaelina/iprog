db92c3f05c216be63c8ed84bdc442d85b9e546c5
togrba <togrba@gmail.com>
2019-01-28 16:32:52 +0100

    Updated template literals

--- dinnerplanner-html-master/js/view/01homeView.js
+++ dinnerplanner-html-master/js/view/01homeView.js
@@ -2,6 +2,14 @@ var HomeView = function(container, model) {
 
   this.createnew = container.find("#createnew");    // create new dinner button
 
-  container.append('<p id="homepageparagraph">Lorem ipsum dolordenifalekfn sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'+
-  '<div class="centered"><button id="createnew" class="button"><b>Create new dinner</b></button></div>');
+  container.append(`
+    <p id="homepageparagraph">
+    Lorem ipsum dolordenifalekfn sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <div class="centered">
+      <button id="createnew" class="button">
+      <b>Create new dinner</b>
+      </button>
+    </div>
+  `);
 }
