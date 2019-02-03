e1e6dfe53503dec24aa5cf1b0c4e3feb5ec623cb
Melissa Obrou <melissaobrou@Melissas-MacBook-Pro.local>
2019-02-03 23:41:37 +0100

    views are up again

--- dinnerplanner-html-master/js/app.js
+++ dinnerplanner-html-master/js/app.js
@@ -18,8 +18,8 @@ $(function() {
 	//var generalStateController = new GeneralStateController($("#welcomeView,#searchDishView,#dishDetailsView,#dinnerOverview,#dinnerPrintoutView,#sidebarView,#sidebarView-2,#dinnerStatusView,#dinnerStatusView-2"), model);
 	//var sidebarController = new SidebarController($("#sidebarView"), model);   // to use when cortrollers are in place
 	var sidebarController = new SidebarController(model, sidebarView, sidebarView-2, this);		// remove when controller are in place
-	var statusController = new StatusController(model, dinnerStatusView, dinnerStatusView-2, this))  // fix
-	var homeController = new HomeController(model, welcomeView, this))  // fix
+	var statusController = new StatusController(model, dinnerStatusView, dinnerStatusView-2, this)  // fix
+	var homeController = new HomeController(model, welcomeView, this)  // fix
 	var searchDishController = new SearchDishController(model, searchDishView, this);
 	var detailsController = new DetailsController(model, dishDetailsView, this)  // fix
 	//var searchController = new SearchController($(""))  // fix
